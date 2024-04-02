14d179d4e2ede9a0ae1f3b43044ccb7ef985a4f2
VidyaBipin <134671559+VidyaBipin@users.noreply.github.com>
2024-04-02 15:47:41 +0530

    Update new_relic-secret-keyExcluded.py

--- new_relic-secret-keyExcluded.py
+++ new_relic-secret-keyExcluded.py
@@ -1,3 +1,9 @@
 new_relic_license_key: "abc123xyz",
 new_relic_api_key="myNewRelicAPIKey123",
 "new_relic_admin_api_key": 'admin_api_key_value'
+
+{
+  "new_relic_license_key": "abc123xyz",
+  "new_relic_api_key": "myNewRelicAPIKey123",
+  "new_relic_admin_api_key": "admin_api_key_value"
+}
