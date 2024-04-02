df42adb2b8aa89c6f5dd57af3afdb69250403cc4
VidyaBipin <134671559+VidyaBipin@users.noreply.github.com>
2024-04-02 12:15:46 +0530

    Add files via upload

---
+++ trustme-ai-secret-source-js.js
@@ -0,0 +1,7 @@
+Username = "JohnDoe123"
+Secret= "MySecret123"
+Password = 'StrongPass789'
+User_name = "jane_doe"
+New_relic_license_key = "abc123xyz"
+Api_key = 'my_api_key'
+Access_key = 'secure_access_key'
