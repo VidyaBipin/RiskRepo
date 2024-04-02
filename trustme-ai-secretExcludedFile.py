64bbe2c66a9fe34bb41911cbed85766250ba09a3
VidyaBipin <134671559+VidyaBipin@users.noreply.github.com>
2024-04-02 15:52:59 +0530

    Update trustme-ai-secretExcludedFile.py

--- trustme-ai-secretExcludedFile.py
+++ trustme-ai-secretExcludedFile.py
@@ -3,3 +3,11 @@ access_key: 'access_key_value',
 'secretkey'   : "mySecretKey",
 "Api_Key"="MyApiKey123",
 "secret_key": "super_secret_key",
+
+{
+  "password": "StrongPassword123",
+  "access_key": "access_key_value",
+  "secretkey": "mySecretKey",
+  "Api_Key": "MyApiKey123",
+  "secret_key": "super_secret_key"
+}
