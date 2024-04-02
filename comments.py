eb23ae69152231ffaf2df51864afc10f8005096d
VidyaBipin <134671559+VidyaBipin@users.noreply.github.com>
2024-04-02 17:37:45 +0530

    Add files via upload
    Python file with comments which satifies the  id: trustme-ai-secret-source-python

---
+++ comments.py
@@ -0,0 +1,7 @@
+#username = "john_doe"
+#password = "s3cr3tP@ssw0rd"
+##api_key = "abcd1234"
+#access_key = 'xyz9876'
+#user_name= "alice_smith"
+##redis_password="RedisP@ss123"
+#new_relic_license_key = "nr-license-key-123"
