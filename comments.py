be386f48150ce6ddb19a8ef62eb2aca6a73b406d
VidyaBipin <134671559+VidyaBipin@users.noreply.github.com>
2024-04-02 17:47:07 +0530

    Update comments.py

--- comments.py
+++ comments.py
@@ -1,7 +1,8 @@
-#username = "john_doe"
-#password = "s3cr3tP@ssw0rd"
-##api_key = "abcd1234"
-#access_key = 'xyz9876'
-#user_name= "alice_smith"
-##redis_password="RedisP@ss123"
-#new_relic_license_key = "nr-license-key-123"
+#username 
+#password 
+##api_key 
+#access_key 
+
+#user_name 
+##redis_password
+#new_relic_license_key 
